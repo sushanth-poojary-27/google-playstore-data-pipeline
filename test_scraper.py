bd348af109a8f95e3866acb9b7bb8a50d1ee8480
sushanth-poojary-27 <poojarysushanth86@gmail.com>
2026-08-07 23:07:20 +0530

    scrapped 4 games data using import

--- test_scraper.py
+++ test_scraper.py
@@ -1,1 +1,14 @@
-print("Pipeline Starting...")
+from google_play_scraper import app
+
+game_ids = ["com.supercell.clashofclans" , "com.pubg.imobile" , "com.supercell.clashroyale" , "com.nekki.shadowfight"]
+
+print("====Multi Game Details====")
+for game_id in game_ids:
+    game_details = app(game_id)
+    print("Game Name", game_details['title'])
+    print("Game Developer" ,game_details['developer'])
+    print("Game Rating", game_details['contentRating'])
+    print("Total Installs:", game_details['installs'])
+    print("Genre:", game_details['genre'])
+    print("Ad Supported:", game_details['adSupported'])
+    print("\n=========================")
