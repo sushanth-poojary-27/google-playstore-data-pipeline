41749b0c00925d241ef3d7973c76b62bdc83f33e
sushanth-poojary-27 <poojarysushanth86@gmail.com>
2026-08-07 23:30:32 +0530

    Implemented Pandas to save scraped multi-game data to CSV

--- test_scraper.py
+++ test_scraper.py
@@ -1,14 +1,24 @@
 from google_play_scraper import app
+import pandas as pd
 
 game_ids = ["com.supercell.clashofclans" , "com.pubg.imobile" , "com.supercell.clashroyale" , "com.nekki.shadowfight"]
-
+all_games_data = []
 print("====Multi Game Details====")
 for game_id in game_ids:
     game_details = app(game_id)
-    print("Game Name", game_details['title'])
-    print("Game Developer" ,game_details['developer'])
-    print("Game Rating", game_details['contentRating'])
-    print("Total Installs:", game_details['installs'])
-    print("Genre:", game_details['genre'])
-    print("Ad Supported:", game_details['adSupported'])
+    extracted_data = {
+        'title': game_details['title'], 
+        'developer': game_details['developer'],
+        'contentRating': game_details['contentRating'],
+        'installs': game_details['installs'],
+        'genre': game_details['genre'],
+        'ad supported': game_details['adSupported']
+    }
+    all_games_data.append(extracted_data)   
     print("\n=========================")
+    print("Scraping Completed saving to CSV file")
+
+df = pd.DataFrame(all_games_data)
+df.to_csv('my_game_details.csv', index=False)
+
+print("Data saved to my_game_details.csv")
